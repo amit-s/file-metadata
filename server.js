dcd71eba2ed58ac320a52b37f449abd40193c0da
amit <shettya@gmail.com>
2017-02-20 17:54:08 -0800

    create file upload form. configure multer -- working

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 'use strict';
-
+let multer = require('multer');
+let upload = multer({dest: 'uploads/'});
 let express = require('express');
 let app = express();
 let port = process.env.PORT || 3000;
@@ -8,9 +9,18 @@ app.set('port', port);
 app.set('view engine', 'pug');
 app.use('/public/styles', express.static(__dirname + '/public/styles'));
 
+
+
 app.route('/')
 	.get(function(req,res){
 		res.render('index');
+	});
+
+app.route('/fileupload')
+	.post(upload.single('user-file'), function(req,res){
+		//console.log(req.body);
+		console.log(req.file);
+		res.send('POST!!!!')
 	});
 
 app.listen(app.get('port'),function(){
